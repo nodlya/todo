0f9a6d9976c10450214763b27f901a1c3614ffe6
nodlya <nadya-popova-02@mail.ru>
2023-05-11 11:29:54 +0500

    fixed save and cancel editing

--- src/form.jsx
+++ src/form.jsx
@@ -6,6 +6,7 @@ const Form = () => {
   const [text, setTaskText] = useState('')
   const [tasks, setTasks] = useState([])
 
+
   const handleInputChange = (event) => {
     setTaskText(event.target.value);
   }
@@ -17,18 +18,20 @@ const Form = () => {
     setTaskText('')
   }
 
-  const handleDelete = (task) => {
-    setTasks(tasks.filter((_, i) => i !== task));
-  };
-
   return (<><form onSubmit={handleSubmit}>
     <input className='text' type='text' value={text} onChange={handleInputChange} />
     <input className='send' type='submit' />
   </form>
     <input type='button' value='удалить все' onClick={() => setTasks([])} />
-    {tasks.map((task, index) => (
-      <Task key={index} info={task.text} onDelete={() => handleDelete(index)} setTasks={setTasks} />
-    ))}
+        {tasks.map((task, index) => (
+            <Task
+                key={index}
+                index={index}
+                info={task.text}
+                startText={task.text}
+                setTasks={setTasks}
+            />
+        ))}
   </>
   )
 }
--- src/task.jsx
+++ src/task.jsx
@@ -1,24 +1,51 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useRef } from "react";
 import editIcon from "./images/edit.png";
 import deleteIcon from "./images/delete.png";
+import cancelIcon from "./images/cancel.png";
+import saveIcon from "./images/save.png";
 import "./styles/task.scss"
-import Edit from "./edit";
 
 const Task = (props) => {
 
     const [toRemove, remove] = useState(false);
     const [toEdit, setToEdit] = useState(false);
+    const [text, setText] = useState(props.info);
+    const [startText, setStartText] = useState(props.startText);
+
+    const inputRef = useRef(null);
 
     const handleEditClick = () => {
         setToEdit(true);
     }
 
-    const handleEditSave = (text, key) => {
+    const handleEditSave = () => {
         setToEdit(false);
         props.setTasks(prevTasks =>
-            prevTasks.map((task, index) => index === key ? { ...task, text: text } : task)
+            prevTasks.map((task, index) => index === props.index ? { ...task, text: text } : task)
         );
+        setStartText(text)
+    }
+
+    useEffect(() => {
+        if (toEdit) {
+            inputRef.current.focus();
+        }
+    }, [toEdit]);
+
+    const handleInputChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            handleEditSave();
+        }
+    }
+
+    const handleEditCancel = () =>{
+        setText(startText)
+        setToEdit(false);
     }
 
     if (toRemove) {
@@ -26,23 +53,35 @@ const Task = (props) => {
     }
 
     if (toEdit) {
+        
         return (
-            <Edit
-                text={props.info}
-                index={props.index}
-                onCancel={() => setToEdit(false)}
-                onSave={handleEditSave}
-            />
+            <div className="task" key={props.index}>
+                <input type="checkbox" />
+                <input
+                    className="task-text"
+                    type="text"
+                    value={text}
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+                    ref={inputRef}
+                />
+                <button className="icon" onClick={handleEditCancel}>
+                    <img src={cancelIcon} alt='cancel' />
+                </button>
+                <button className="icon" onClick={handleEditSave}>
+                    <img src={saveIcon} alt='save' />
+                </button>
+            </div>
         );
     }
 
-    return (<div className="task">
+    return (<div className="task" key={props.index}>
         <input type="checkbox" />
         <p>{props.info}</p>
-        <button className="edit-icon">
+        <button className="icon">
             <img src={editIcon} alt='edit' onClick={handleEditClick} />
         </button>
-        <button className="delete-icon" onClick={() => remove(true)}>
+        <button className="icon" onClick={() => remove(true)}>
             <img src={deleteIcon} alt='delete' />
         </button>
     </div>)
